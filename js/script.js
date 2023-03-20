d2d3a87b7504b6448d63f8d0e4ac82c7945bc64a
Imotytskiy <Imotytskiy@gmail.com>
2023-03-20 09:59:40 +0100

    add changing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function showIt() {
     document.getElementById("hid").style.visibility = "visible";
 }
-setTimeout("showIt()", 3000); // after 3 seconds
-let timeoutId = setTimeout("showIt()", 3000); // store the timeout ID in a variable
+setTimeout("showIt()", 30000); // after 3 seconds
+let timeoutId = setTimeout("showIt()", 30000); // store the timeout ID in a variable
 
 function closeIt() {
     clearTimeout(timeoutId); // clear the timeout to prevent the popup from showing again
